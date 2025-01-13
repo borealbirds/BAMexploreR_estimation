49f3d24b72c12fe842ae2d3b09b69d0c25317511
mannfred boehm <mannfred.boehm@ubc.ca>
2025-01-13 12:02:18 -0800

    start v5 cov importance script

--- interpretation/PrepareDataCovariateImportanceV5.R
+++ interpretation/PrepareDataCovariateImportanceV5.R
@@ -1,12 +1,12 @@
 # ---
-# title: National Models 4.0 - data organization for analysing relative influence of covariates and species traits
+# title: National Models 5.0 - data organization for analysing relative influence of covariates and species traits
 # author: Mannfred Boehm
-# created: November 19, 2024
+# created: January 13, 2025
 # ---
 
 #NOTES################################
 
-# This script extracts covariate contributions to model predictions (VERSION 4), and also synthesizes various trait databases.
+# This script extracts covariate contributions to model predictions (VERSION 5).
 
 # The outputs will be analysed for identifying covariates of importance across several metrics (e.g. BCR, ecology, etc.).
 
@@ -17,6 +17,7 @@
 library(gbm)
 library(parallel)
 library(tidyverse)
+
 
 
 #2. setup local or cluster----
@@ -45,7 +46,6 @@ if(cc){root <- "/home/mannfred/scratch"}
 
 print(paste("* currently working out of ", root, " *"))
 
-
 # attach packages on clusters
 # `clusterEvalQ` evaluates a literal expression on each cluster node. 
 print("* Loading packages on workers *")
@@ -55,19 +55,20 @@ invisible(clusterEvalQ(cl, library(tidyverse)))
 
 
 #3. access gbm objects and append spp/bcr/boot info----
-gbm_objs <- list.files(file.path(root, "v4_bootstraps"), pattern = "^gnmboot-.*\\.RData$", full.names = TRUE)
+gbm_objs <- list.files(file.path(root, "v5_bootstraps"), pattern = "*\\.R", full.names = TRUE)
 # (\(x) x[1:3])()
 print(paste("* Found", length(gbm_objs), " files *"))
 print(head(gbm_objs))
 
 
+
+
 #4. extract the species (FLBC), BCR, and bootstrap replicate from `gbm_objs`----
-sample_id <-
-  gbm_objs |>
-  sub("^.*gnmboot-", "", x = _) |>
-  sub("\\.RData", "", x = _) |>
-  stringr::str_split_fixed(pattern="-", n=3) |>
-  tibble::as_tibble() |>
+sample_id <- 
+  gbm_objs |> 
+  stringr::str_split_fixed(pattern="_", n=3) |> 
+  gsub("\\.R", "", x = _) |>
+  tibble::as_tibble() |> 
   magrittr::set_colnames(c("spp", "bcr", "boot"))
 
 if (exists("sample_id")) {
@@ -75,11 +76,14 @@ if (exists("sample_id")) {
 } else {message("* sample_id not constructed *")}
 
 
+
 #5. create master dataframe with indexing info----
 # then, convert to list with each dataframe row as a list element 
 # (easier to deal with a list for looping/`apply`ing)
 gbm_data <- tibble(file_path = gbm_objs, spp = sample_id$spp, bcr = sample_id$bcr, boot = sample_id$boot) 
 gbm_list <- split(x=gbm_data, f=seq(nrow(gbm_data)))
+
+
 
 
 #6. define function that creates `bam_covariate_importance` data frame----
@@ -98,7 +102,7 @@ compute_var_importance <- function(gbm_object) {
     load(gbm_object[["file_path"]])
     
     # validate gbm object
-    if (!exists("out") || is.null(out$n.trees) || out$n.trees <= 0) {
+    if (!exists("b.i") || is.null(b.i$n.trees) || b.i$n.trees <= 0) {
       warning(paste("Invalid GBM model in file:", gbm_object["file_path"]))
       return(NULL)
     } else {
@@ -108,11 +112,10 @@ compute_var_importance <- function(gbm_object) {
     # compute variable importance
     # to reduce file size, filter for rel.inf >= 1
     importance_df <-
-      out |>
+      b.i |>
       gbm::summary.gbm(plotit = FALSE) |>
       as_tibble() |>
-      dplyr::bind_cols(x = _, c(gbm_object["spp"], gbm_object["bcr"], gbm_object["boot"])) |>
-      dplyr::filter(rel.inf >= 1)
+      dplyr::bind_cols(x = _, c(gbm_object["spp"], gbm_object["bcr"], gbm_object["boot"]))
     
     message("Successfully extracted `rel.inf` from: ", gbm_object["file_path"])
     return(importance_df)
@@ -121,12 +124,12 @@ compute_var_importance <- function(gbm_object) {
     rm(out)
     gc()
     
-  # give instructions to tryCatch if it runs into an error
+    # give instructions to tryCatch if it runs into an error
   }, error = function(e) {
     warning(paste("Error processing", gbm_object["file_path"], ":", e$message))
     return(NULL) 
     
-    }) # close tryCatch
+  }) # close tryCatch
   
 } # close compute_var_importance()
 
@@ -139,8 +142,8 @@ clusterExport(cl, c("root", "gbm_list", "compute_var_importance"))
 
 #8. run the covariate importance function in parallel----
 print("* running `compute_var_importance` in parallel` *")
-bam_covariate_importance_v4 <- parLapply(cl, gbm_list, compute_var_importance)
-saveRDS(bam_covariate_importance_v4, file=file.path(root, "bam_covariate_importance_v4.rds"))
+bam_covariate_importance_v5 <- parLapply(cl, gbm_list, compute_var_importance)
+saveRDS(bam_covariate_importance_v5, file=file.path(root, "bam_covariate_importance_v5.rds"))
 
 
 #9. stop the cluster----
@@ -153,12 +156,13 @@ if(cc){ q() }
 #10. post-cluster data cleanup (executed on local machine)----
 
 # remove NULL (invalid model) entries
-bam_covariate_importance_v4_nonull <- bam_covariate_importance_v4[!sapply(bam_covariate_importance_v4, is.null)]
+bam_covariate_importance_v5_nonull <- bam_covariate_importance_v5[!sapply(bam_covariate_importance_v5, is.null)]
 
 # reduce list of dataframes into a single dataframe
 # took about 15 minutes with a list of 12808 tibbles
-covariate_importance_merged <- suppressMessages(purrr::reduce(bam_covariate_importance_v4_nonull, full_join))
+covariate_importance_merged <- suppressMessages(purrr::reduce(bam_covariate_importance_v5_nonull, full_join))
 
+# THIS IS V4: GET V5!!!
 # import extraction lookup table to obtain covariate classes----
 # (for appending to covariate importance data)
 # lookup table is missing "Year" and "Method", so manually adding here
@@ -171,25 +175,13 @@ nice_var_names <-
 covariate_importance_zeroed <-
   covariate_importance_merged |>
   dplyr::mutate(boot = as.integer(boot)) |> # change `chr` to `int`
-  tidyr::complete(spp, bcr, var, boot = 1:32, fill = list(rel.inf = 0)) |>  # create rows for every spp x bcr x var x bootstrap combo; fill missing rows with 0
   dplyr::group_by(spp, bcr, var) |>
   dplyr::summarise(mean_rel_inf = mean(rel.inf, na.rm = TRUE),  # calculate mean across all bootstraps (including 0s)
                    sd_rel_inf = sd(rel.inf, na.rm = TRUE),      # calculate standard deviation across all bootstraps (including 0s)
                    n_boots = sum(rel.inf > 0)) |>    # count the number of bootstraps where the variable had non-zero rel.inf
-  dplyr::filter(mean_rel_inf > 0) |> # filter out spp x bcr x var tuples that don't have rel.inf data
+  dplyr::filter(mean_rel_inf >= 1) |> # filter out spp x bcr x var tuples with mean rel.inf <= 1
   dplyr::left_join(nice_var_names, by = "var") |> # append more interpretable variable names
   dplyr::arrange(spp, bcr, desc(mean_rel_inf))
-
-saveRDS(covariate_importance_zeroed, file=file.path(root, "bam_covariate_importance_v4.rds"))
-
-
-# `bam_covariate_importance_v4` is a `data.frame` with rows as the mean relative influence (of 32 bootstraps) of a model covariate for a given species x BCR. 
-# There are 7 columns: `spp` gives the Four-Letter Bird Code indicating the species, `bcr` is the Bird Conservation Region, and `var` is the covariate.
-# `mean_rel_inf` and `sd_rel_inf` are the mean and standard deviation of the covariate relative influence across 32 bootstraps. When a covariate was absent from
-# one (or more) of the 32 bootstraps, it was considered as having a relative influence of zero for that bootstrap (thus lowering the mean). 
-# Relative influence was reported only when the mean was greater than or equal to 1. 
-# `n_boots` indicates how many bootstraps the given covariate appeared in. `var_class` denotes a broad variable class to which the covariate belongs. 
-# The code used to generate this dataset can be found in "PrepareDataCovariateImportanceV4.R"
 
 
 
