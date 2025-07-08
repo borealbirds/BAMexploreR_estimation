2b897fbf0a59017f74c8ec139303c21f6dbddefb
mannfred boehm <mannfred.boehm@ubc.ca>
2025-07-08 13:21:46 -0700

    redo loop in prepare data for partial dep

--- interpretation/PrepareDataPartialDependanceV5.R
+++ interpretation/PrepareDataPartialDependanceV5.R
@@ -55,23 +55,48 @@ print(paste("* Found", length(gbm_objs), " files *"))
 print(head(gbm_objs))
 
 # import covariate importance data from "PrepareDataCovariateImportanceV5.R"
+# load(file="C:/Users/mannf/Proton Drive/mannfredboehm/My files/Drive/BAMexploreR/data-raw/bam_covariate_importance_v5.rda")
 load(file="/home/mannfred/scratch/bam_covariate_importance_v5.rda")
 print("* loading covariate importance data *")
 head(bam_covariate_importance_v5)
 
-# process_gbm takes a path to a .Rdata file containing a list of bootstrap models (`b.list`)
+
+# group_keys() finds the names of the unique species x bcr x var tuples in `bam_covariate_importnance`
+boot_group_keys <- 
+  bam_covariate_importance_v5 |> 
+  dplyr::group_by(bcr, spp, var) |> 
+  dplyr::group_keys()
+
+
+# evaluate_grid_from_model takes a path to a .Rdata file containing a list of bootstrap models (`b.list`)
 # and returns a nested list:
-# top-level: each element is a bootstrap replicate (i.e., each model in `b.list`)
-# second-level list: one `data.frame` per covariate, containing the evaluation grid from `plot.gbm()`
-evaluate_grid_from_model <- function(obj_path) {
+# top-level: each element is a species x bcr tuple 
+# second-level: 32 elements, each element is a list representing a bootstrap
+# third-level: each element is a `data.frame` containing a covariate's evaluation grid from `plot.gbm()`
+
+evaluate_grid_from_model <- function(obj_path, target_vars) {
   
-  load(obj_path)  # loads b.list
+  # load list of 32 bootstraps for current species x bcr tuple
+  load(obj_path)  
   
-  boot_pts <- list()
+  # index current species x bcr tuple for searching in `bam_covariate_importance`
+  spp <- stringr::str_extract(obj_path, "[A-Z]{4}")
+  bcr <- stringr::str_extract(obj_path, "can[0-9]+")
+  
+  # match the current species x bcr in `bam_covariate_importance` and
+  # filter out any covariates that aren't associated with the current tuple
+  relevant_vars <- 
+    target_vars |> 
+    dplyr::filter(spp == !!spp, bcr == !!bcr) |> 
+    dplyr::pull(var) |> 
+    unique()
+  
   
   
   # find evaluation points (`data.frame`) for every covariate (indexed by `r`)
   # this can sped up by reducing `continuous.resolution=100` in `plot.gbm()`
+  boot_pts <- list()
+  
   for (b in seq_along(b.list)) {
     model <- b.list[[b]]
     
@@ -80,8 +105,11 @@ evaluate_grid_from_model <- function(obj_path) {
       pts[[r]] <- plot.gbm(x = model, return.grid = TRUE, i.var = r, type = "response")
     }
     
+    names(pts) <- model$var.names
     boot_pts[[b]] <- pts
   }
+  
+  names(boot_pts) <- paste0("bootstrap_", seq_along(b.list))
   
   return(boot_pts)
 }
@@ -111,19 +139,12 @@ stopCluster(cl)
 
 #9. save boot_pts----
 print("* saving boot_pts_v5.rds  *")
-saveRDS(boot_pts, file=file.path(root, "boot_pts_v5.rds"))
+saveRDS(boot_pts, file="/home/mannfred/scratch/boot_pts_v5.rds")
 
-
-# from the family of sets (BCRs, species, covariates) 
-# group_keys() finds the names of the unique tuples
-boot_group_keys <- 
-  bam_covariate_importance_v5 |> 
-  dplyr::group_by(bcr, spp, var) |> 
-  dplyr::group_keys()
 
 
 # create an index from `gbm_objs` containing the 
-# species (FLBC), BCR, and bootstrap replicate
+# species, BCR, and bootstrap replicate
 sample_id <- 
   tibble::tibble(
     spp = str_extract(gbm_objs, "/([^/]+)/[^/]+\\.Rdata$") |> str_extract("[A-Z]{4}"),
@@ -134,11 +155,12 @@ sample_id <-
 
 
 # check that sample_id and boot_pts have same length
-stopifnot(length(sample_id) == length(boot_pts))
+stopifnot(nrow(sample_id) == length(boot_pts)*32)
 
 
 # for every zth bcr x species x var tuple (rows in `boot_group_keys`):
-# search for the relevant bootstrap predictions in `boot_pts` by matching species and bcr at the top level of `boot_pts` then matching `var` at the second level
+# search for the relevant bootstrap predictions in `boot_pts` by matching species and 
+# bcr at the top level of `boot_pts` then matching `var` at the second level
 # then, enter bootstrap dataframes for a given bcr x species x var tuple as a sub-element of [[z]]
 # output: a list of lists where the top level elements are species x bcr x var tuples, 
 # and the second-level elements are dataframes of that tuple's bootstrapped model predictions
@@ -150,7 +172,7 @@ for (z in 1:nrow(boot_group_keys)){
   key_z <- boot_group_keys[z,]
   
   # `sample_id` and `boot_pts` have the same length and order (they are derived from `gbm_objs`)
-  # so we can identify elements in the latter using info from the former
+  # so we can identify the species x bcr in the latter using info from the former
   # search for all bootstraps for a given bcr x sample tuple
   boot_pts_index <- which(sample_id$bcr == key_z$bcr & sample_id$spp == key_z$spp)
   
@@ -179,5 +201,45 @@ for (z in 1:nrow(boot_group_keys)){
   cat(paste("\riteration", z))
   Sys.sleep(0.001)
 }
+
+
+
+boot_pts_sorted <- 
+  
+  # spp_bcr_entry inputs a list of 32 bootstraps for a species x bcr tuple (top level of boot_pts)
+  # key_prefix is the name of species x bcr tuple
+  purrr::imap(boot_pts, function(spp_bcr_bootstraps, spp_bcr_name) {
+    
+    # for every bootstrap (for a given spp x bcr tuple)
+    # define a new function that requires a covariate dataframe
+    purrr::imap(spp_bcr_bootstraps, function(vars_list, boot_name) {
+    
+      # for every covariate build a list:
+      # the top element is the current species x bcr x covariate
+      # the second-level elements are dataframes, one per 32 bootstraps
+      purrr::imap(vars_list, function(df, var) {
+      
+        list(key = paste(spp_bcr_name, var, sep = "_"),
+             boot = boot_name,
+             df = df)
+      
+      # flatten() each level to turn nested lists into a single vector of lists
+      }) 
+  
+    }) |> purrr::flatten()
+
+  }) |> flatten()
+
+# Then group by tuple key:
+
+boot_pts_sorted <- 
+  boot_pts_sorted |> 
+  purrr::flatten() |> 
+  split(x = _, f = purrr::map_chr(., "key")) |> 
+  purrr::map(~ purrr::set_names(purrr::map(., "df"), nm = paste0("bootstrap_", seq_along(.))))
+
+boot_pts_sorted <- split(map(boot_pts_sorted, "df"),
+                         map_chr(boot_pts_sorted, "key"))
+
 
 saveRDS(boot_pts_sorted, file=file.path(root, "boot_pts_sorted_v5.rds"))
